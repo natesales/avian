067e55ad06f7bc578846557d4290a75bb6703fdd
Nate Sales <nate@natesales.net>
2021-10-16 00:22:21 -0700

    feat: add pose tank drive

--- main.py
+++ main.py
@@ -20,11 +20,16 @@ DRAW = True  # Display graphics on screen
 DRAW_TO_ORIGIN = True
 
 SD_DEFAULTS = {
-    "avian/max_num_hands": 1,
+    "avian/max_num_hands": 2,
     "avian/pinch_proximity_radius": 7,
     "avian/invert_horizontal": False,
     "avian/invert_vertical": False,
     "avian/black_background": False,
+
+    # Deadzones are in pixels above and below.
+    # Not cumulative; the space between {forward,backward}_thresh is 2*deadzone)
+    "avian/tank_middle_deadzone": 30,
+    "avian/tank_top_bottom_deadzone": 50,
 
     # Immutable
     "avian/fps": 0,
@@ -83,6 +88,21 @@ while True:
         h, w, _ = img.shape
         img = numpy.zeros((h, w, 3), numpy.uint8)
 
+    # Draw segment lines
+    color = (255, 255, 255)
+    size = 3
+    middle_deadzone = int(sd.get("avian/tank_middle_deadzone"))
+    top_bottom_deadzone = int(sd.get("avian/tank_top_bottom_deadzone"))
+    forward_thresh = int(h / 2) - middle_deadzone
+    backward_thresh = int(h / 2) + middle_deadzone
+    forward_max = top_bottom_deadzone
+    backward_max = h - top_bottom_deadzone
+    cv2.line(img, (int(w / 2), 0), (int(w / 2), h), color, size)  # Vertical half
+    cv2.line(img, (0, forward_thresh), (w, forward_thresh), color, size)  # Forward area
+    cv2.line(img, (0, backward_thresh), (w, backward_thresh), color, size)  # Backward area
+    cv2.line(img, (0, forward_max), (w, forward_max), color, size)  # Forward max
+    cv2.line(img, (0, backward_max), (w, backward_max), color, size)  # Backward max
+
     if results.multi_hand_landmarks:
         sd.set("avian/detected_hands", len(results.multi_hand_landmarks))
         for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
@@ -91,8 +111,8 @@ while True:
             if results.multi_handedness:
                 handedness = results.multi_handedness[idx].classification[0].label.lower()
 
-            # Draw hand matrix
             if DRAW:
+                # Draw hand matrix
                 mediapipe.solutions.drawing_utils.draw_landmarks(img, hand_landmarks,
                                                                  mediapipe.solutions.hands.HAND_CONNECTIONS)
 
@@ -136,26 +156,35 @@ while True:
                 if g != gesture:
                     sd.set(f"avian/{handedness}_{g}", False)
 
-            # draw line to origin
-            if DRAW_TO_ORIGIN and len(right_hand_poses) > 0:
-                # print(right_hand_poses[0])
-                lm = hand_landmarks.landmark[mediapipe.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
-                angle = numpy.arctan((-lm.y + 0.5) / (lm.x - 0.5))
-                if lm.x < 0.5:
-                    angle += numpy.pi
-                if angle < 0:
-                    angle = 2 * numpy.pi + angle
-                angle *= (180 / numpy.pi)
-                cv2.line(img, (int(lm.x * w), int(lm.y * h)), (int(w / 2), int(h / 2)), (255, 255, 255), 10)
-                cv2.putText(img, str(angle), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # # draw line to origin
+            # if DRAW_TO_ORIGIN and len(right_hand_poses) > 0:
+            #     # print(right_hand_poses[0])
+            #     lm = hand_landmarks.landmark[mediapipe.solutions.hands.HandLandmark.INDEX_FINGER_TIP]
+            #     angle = numpy.arctan((-lm.y + 0.5) / (lm.x - 0.5))
+            #     if lm.x < 0.5:
+            #         angle += numpy.pi
+            #     if angle < 0:
+            #         angle = 2 * numpy.pi + angle
+            #     angle *= (180 / numpy.pi)
+            #     cv2.line(img, (int(lm.x * w), int(lm.y * h)), (int(w / 2), int(h / 2)), (255, 255, 255), 10)
+            #     cv2.putText(img, str(angle), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            #
+            # if len(right_hand_poses) > 0:
+            #     startX, startY = right_hand_poses[len(right_hand_poses) - 1]
+            #     endX, endY = right_hand_poses[0]
+            #     distance_traveled = endX - startX
+            #     if distance_traveled > 100:
+            #         swiped_distance += 10
+            #     # print(p_time)
 
-            if len(right_hand_poses) > 0:
-                startX, startY = right_hand_poses[len(right_hand_poses) - 1]
-                endX, endY = right_hand_poses[0]
-                distance_traveled = endX - startX
-                if distance_traveled > 100:
-                    swiped_distance += 10
-                # print(p_time)
+            # Tank drive
+            lm_y = hand_landmarks.landmark[mediapipe.solutions.hands.HandLandmark.INDEX_FINGER_TIP].y * h
+            if lm_y < forward_thresh:
+                sd.set(f"avian/{handedness}_tank", (lm_y - forward_thresh) / (forward_max - forward_thresh))
+            elif lm_y > backward_thresh:
+                sd.set(f"avian/{handedness}_tank", -1 * ((lm_y - backward_thresh) / (backward_max - backward_thresh)))
+            else:  # Deadzone
+                sd.set(f"avian/{handedness}_tank", 0)
 
     else:  # No hands detected
         sd.set("avian/detected_hands", 0)
@@ -177,6 +206,10 @@ while True:
     if DRAW:
         cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
         cv2.imshow("Image", img)
-        if cv2.waitKey(1) == 27:
+
+        key = cv2.waitKey(1)
+        if key == 27:  # escape for exit
             cv2.destroyAllWindows()
             exit(0)
+        elif key == 105:  # i for invert
+            sd.set("avian/invert_horizontal", not sd.get("avian/invert_horizontal"))
